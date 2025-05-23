cb8da2b0a275eea1328cdf2d27a6fc6bfa5b350f
Plunne <plunne.celeste@gmail.com>
2025-05-24 00:37:31 +0200

    Money alises

--- Money.py
+++ Money.py
@@ -12,33 +12,53 @@ def money(in_fromMoney, in_value, in_toMoney):
 def toUSD(in_value):
   return in_value * USD
 
+tousd = toUSD
+
 def fromUSD(in_value):
   return in_value / USD
+
+fromusd = fromUSD
 
 # CAD
 def toCAD(in_value):
   return in_value * CAD
 
+tocad = toCAD
+
 def fromCAD(in_value):
   return in_value / CAD
+
+fromcad = fromCAD
 
 # GBP  
 def toGBP(in_value):
   return in_value * GBP
 
+togpb = toGBP
+
 def fromGBP(in_value):
   return in_value / GBP
+
+fromgpb = fromGBP
 
 # CHF
 def toCHF(in_value):
   return in_value * CHF
 
+tochf = toCHF
+
 def fromCHF(in_value):
   return in_value / CHF
+
+fromchf = fromCHF
 
 # JPY
 def toJPY(in_value):
   return in_value * JPY
 
+tojpy = toJPY
+
 def fromJPY(in_value):
   return in_value / JPY
+
+fromjpy = fromJPY
